3018f5c7b1d98309244e404f48496a4cec6c0ddf
Natan <xpenatan@gmail.com>
2023-04-05 18:24:04 -0300

    clean up code

--- standalonelib/src/main/java/com/sample/localaar/myawesomemodule3/AwesomeStandAloneModule.kt
+++ standalonelib/src/main/java/com/sample/localaar/myawesomemodule3/AwesomeStandAloneModule.kt
@@ -3,6 +3,6 @@ package com.sample.localaar.myawesomemodule3
 
 open class AwesomeStandAloneModule {
     companion object {
-        val greet = "Hello from StandAloneModule"
+        val greet = "Hello from StandAloneModule 2"
     }
 }
