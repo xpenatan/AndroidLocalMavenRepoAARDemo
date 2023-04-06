8cf9ef02b39bc3f057a994daa0ba631908941f15
Natan <xpenatan@gmail.com>
2023-04-05 22:50:01 -0300

    Remove group requirement

--- feature/feature01/myawesomemodule1/src/main/java/com/sample/localaar/myawesomemodule1/AwesomeModule1.kt
+++ feature/feature01/myawesomemodule1/src/main/java/com/sample/localaar/myawesomemodule1/AwesomeModule1.kt
@@ -1,8 +1,10 @@
 package com.sample.localaar.myawesomemodule1
 
+import com.sample.localaar.myawesomemodule2.AwesomeModule2
+
 
 class AwesomeModule1{
     companion object {
-        val greet = "Hello from AwesomeModule1"
+        val greet = "Hello from AwesomeModule1" + AwesomeModule2.greet
     }
 }
--- feature/feature02/myawesomemodule2/src/main/java/com/sample/localaar/myawesomemodule2/AwesomeModule2.kt
+++ feature/feature02/myawesomemodule2/src/main/java/com/sample/localaar/myawesomemodule2/AwesomeModule2.kt
@@ -1,8 +1,11 @@
 package com.sample.localaar.myawesomemodule2
 
+import com.sample.localaar.myawesomemodule3.AwesomeModule3
+import com.sample.localaar.myawesomemodule3.AwesomeStandAloneModule
+
 
 open class AwesomeModule2{
     companion object {
-        val greet = "Hello from AwesomeModule2"
+        val greet = "Hello from AwesomeModule2" + "\n" + AwesomeModule3.greet + "\n" + AwesomeStandAloneModule.greet
     }
 }
